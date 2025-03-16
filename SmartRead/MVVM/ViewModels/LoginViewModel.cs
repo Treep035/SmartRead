4353259e3bf00b074973d2460114c8cab71ca513
Treep035 <treep037@gmail.com>
2025-03-16 21:28:59 +0100

    access_token and refresh_token creating SecureStorage

--- SmartRead/MVVM/ViewModels/LoginViewModel.cs
+++ SmartRead/MVVM/ViewModels/LoginViewModel.cs
@@ -34,12 +34,40 @@ namespace SmartRead.MVVM.ViewModels
                 await Shell.Current.DisplayAlert("Error", "Debe ingresar un correo y una contraseña.", "OK");
                 return;
             }
+            if (RememberMe)
+            {
+                // Si "Recordarme" está activado, entonces llama a LoginAsync
+                await LoginAsync(Email, Password);
+            }
             _authService.Login();
             await Shell.Current.GoToAsync($"//home");
             // Aquí se simula el login sin consultar ningún servicio:
             //App.IsLoggedIn = true; // Se marca que el usuario inició sesión
 
             await Shell.Current.DisplayAlert("Éxito", "Inicio de sesión exitoso", "OK");
+        }
+
+        public async Task LoginAsync(string username, string password)
+        {
+            // Lógica para autenticar al usuario (simulación)
+            string accessToken = _authService.GenerateRandomToken(32);
+            string refreshToken = _authService.GenerateRandomToken(64);
+
+            // Guardar los tokens
+            await _authService.SaveAccessTokenAsync(accessToken);
+            await _authService.SaveRefreshTokenAsync(refreshToken);
+            Console.WriteLine($"Access Token: {accessToken}");
+            Console.WriteLine($"Refresh Token: {refreshToken}");
+        }
+
+        public async Task GetStoredTokensAsync()
+        {
+            // Recuperar los tokens
+            var accessToken = await _authService.GetAccessTokenAsync();
+            var refreshToken = await _authService.GetRefreshTokenAsync();
+
+            Console.WriteLine($"Access Token: {accessToken}");
+            Console.WriteLine($"Refresh Token: {refreshToken}");
         }
 
         [RelayCommand]
